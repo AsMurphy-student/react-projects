306b2a7d57faa3e40b2e34aa8f85506874415640
AsMurphy-student <AsMurphy-work@protonmail.com>
2024-11-21 20:46:44 -0500

    Fixed build issue and updated header positioning and added dropdown which will be used to change theme

--- mui-testing/src/components/AppBar.tsx
+++ mui-testing/src/components/AppBar.tsx
@@ -6,8 +6,9 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
-import { createTheme } from '@mui/material';
+import { createTheme, MenuItem, Select } from '@mui/material';
 import { Theme, ThemeProvider } from '@emotion/react';
+import Grid from '@mui/material/Grid2';
 
 export default function ButtonAppBar(props: {theme: Theme}) {
 
@@ -27,12 +28,36 @@ export default function ButtonAppBar(props: {theme: Theme}) {
             >
               <MenuIcon />
             </IconButton> */}
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              Ashton Murphy
-            </Typography>
-              <Button sx={{color: "primary.contrastText", textDecorationLine: 'underline'}}>About Me</Button>
-              <Button sx={{color: "primary.contrastText", textDecorationLine: 'underline'}}>Blog Posts</Button>
-              <Button sx={{color: "primary.contrastText", textDecorationLine: 'underline'}}>Credits</Button>
+
+            <Grid container spacing={3} columns={3} size="grow" direction="row" justifyContent="center" alignItems="center">
+              <Grid size={1} sx={{border: 1, py: 'auto'}}>
+                <Typography variant="h6" component="div" align="left"> 
+                  Ashton Murphy
+                </Typography>
+              </Grid>
+              <Grid size={1} sx={{border: 1}} display='flex' justifyContent='center'>
+                <Select
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select"
+                  value={10}
+                  label="Age"
+                  //onChange={handleChange}
+                >
+                  <MenuItem value={10}>Ten</MenuItem>
+                  <MenuItem value={20}>Twenty</MenuItem>
+                  <MenuItem value={30}>Thirty</MenuItem>
+                </Select>
+              </Grid>
+              <Grid size={1} sx={{border: 1}} display='flex' justifyContent='right'>
+                <Button sx={{color: "primary.contrastText", textDecorationLine: 'underline'}}>About Me</Button>
+                <Button sx={{color: "primary.contrastText", textDecorationLine: 'underline'}}>Blog Posts</Button>
+                <Button sx={{color: "primary.contrastText", textDecorationLine: 'underline'}}>Credits</Button>
+              </Grid>
+            </Grid>
+            
+            
+
+              
           </Toolbar>
         </AppBar>
       </Box>
